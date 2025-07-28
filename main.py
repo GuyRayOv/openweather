6a6a2a71dcae46b349eb2a227586285bd53c4c1c
GuyRayOv <guy.ray@gmail.com>
2025-07-28 21:16:36 +0300

    OK

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
 import requests
+import datetime
+import pytz
 
 GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct"
 WEATHER_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
@@ -30,6 +32,11 @@ def get_weather_data_for(location):
     return json2
 
 #================================================================================================
+def get_local_datetime(utc_timestamp, timezone):
+    utc_dt = datetime.datetime.fromtimestamp(utc_timestamp, datetime.UTC).replace(tzinfo=pytz.utc)
+    return utc_dt.astimezone(pytz.timezone(timezone))
+
+
 
 st.title('Weather App')
 
@@ -37,4 +44,4 @@ my_location = st.text_input('Enter a location name', '')
 
 if my_location:
     json = get_weather_data_for(my_location)
-    st.write(f"Current Weather Conditions in {my_location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp']-273.15)}C, {json['current']['humidity']}% humidity")
+    st.write(f"Current Weather Conditions in {my_location} local time {get_local_datetime(json['current']['dt'], json['timezone'])}: {json['current']['weather'][0]['description']}, {int(json['current']['temp']-273.15)}C, {json['current']['humidity']}% humidity")

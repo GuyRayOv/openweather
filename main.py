7c0004a1d08d4981a294f6df94fe561e698338d5
GuyRayOv <guy.ray@gmail.com>
2025-07-28 19:44:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,39 @@
 import streamlit as st
 import requests
 
-"""
-https://api.openweathermap.org/data/3.0/onecall?lat=32.0351&lon=34.5142&appid=c4d9b7d003d31461abe0aec452e24151
-"""
+GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct"
+WEATHER_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
+MY_API_KEY = "c4d9b7d003d31461abe0aec452e24151"
 
-#service
-open_weatehr_base = "https://api.openweathermap.org/data/3.0/onecall"
+#================================================================================================
+def webapi_call(url, params={}):
+    response = requests.get(url, params=params)
+    try: response.raise_for_status()
 
-#my parmas
-my_api_key = "c4d9b7d003d31461abe0aec452e24151"
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return []
+    return response.json()
 
-#applicatio params
-my_lat = 32.0351 #41.878113 #
-my_lon = 34.5142 # -87.629799
+#=================================================================================================
+def get_weather_data_for(location):
+    json1 = webapi_call(GEO_BASE_URL, params={"q": location, "appid": MY_API_KEY})
+    if json1 == []:
+        st.write(f"{my_location} cannt get web infomation")
+        return json1
 
-my_params = {"lat": {my_lat}, "lon": {my_lon}, "appid": {my_api_key}}
+    json2 = webapi_call(WEATHER_BASE_URL, params={"lat":json1[0]["lat"], "lon":json1[0]["lon"], "appid": MY_API_KEY})
+    if json2 == []:
+        st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
 
+    return json2
 
+#================================================================================================
 
 st.title('Weather App')
 
-name = st.text_input('Enter your name', '')
+my_location = st.text_input('Enter a location name', '')
 
-if name:
-    response = requests.get(open_weatehr_base, params=my_params)
-    try: response.raise_for_status()
-    except response.exceptions.HTTPError as e: print(e)
-
-    current = response.json()['current']
-
-    st.write(f'Hello {name}, welcome to the weather app!')
-    st.write(f"Current Weather Conditions at your location: {current['weather'][0]['description']}, {int(current['temp']-273.15)}C, {current['humidity']}% humidity")
-
+json = get_weather_data_for(my_location)
+if not json == []:
+    st.write(f"Current Weather Conditions in {my_location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp']-273.15)}C, {json['current']['humidity']}% humidity")

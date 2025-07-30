87e6589ec851cf6998422229ed1dce2155ad94d4
GuyRayOv <guy.ray@gmail.com>
2025-07-30 14:03:54 +0300

    OK

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ MY_API_KEY = "c4d9b7d003d31461abe0aec452e24151"
 def webapi_call(url, params={}):
     response = requests.get(url, params=params)
     try: response.raise_for_status()
-
     except requests.exceptions.HTTPError as e:
         print(e)
         return []
     return response.json()
-
 #=================================================================================================
 def get_weather_data_for(location):
     json1 = webapi_call(GEO_BASE_URL, params={"q": location, "appid": MY_API_KEY})
@@ -26,8 +24,7 @@ def get_weather_data_for(location):
         return json1
 
     json2 = webapi_call(WEATHER_BASE_URL, params={"lat":json1[0]["lat"], "lon":json1[0]["lon"], "appid": MY_API_KEY})
-    if json2 == []:
-        st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
+    if json2 == []: st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
 
     return json2
 

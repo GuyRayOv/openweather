bb46e2f08bc865544a373600e25162a885e5f35c
GuyRayOv <guy.ray@gmail.com>
2025-07-30 18:40:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from streamlit_folium import st_folium
 #================================================================================================
 GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct"
 WEATHER_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
+WEATHER_ICON_BASE_URL = "http://openweathermap.org/img/wn/01d2x.png"
 MY_API_KEY = "c4d9b7d003d31461abe0aec452e24151"
 FAVORITE_LOCATIONS_FILE = 'favorite_locations.json'
 
@@ -26,32 +27,42 @@ def webapi_call(url, params={}):
 #=================================================================================================
 def get_weather_data_for(location):
     json1 = webapi_call(GEO_BASE_URL, params={"q": location, "appid": MY_API_KEY})
+
     if json1 == []:
         st.write(f"{my_location} cannt get web infomation")
         return json1
 
     json2 = webapi_call(WEATHER_BASE_URL, params={"lat":json1[0]["lat"], "lon":json1[0]["lon"], "appid": MY_API_KEY})
-    if json2 == []: st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
+    if json2 == []:
+        st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
 
     return json2
-
-#=================================================================================================
-def preset_weather_for(location):
-
-    json = get_weather_data_for(location)
-
-    # Print the weather data for the location
-    st.write(f"Weather Conditions in {location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp'] - 273.15)}C, {json['current']['humidity']}% humidity")
-    st.write(f"{get_local_datetime(json['current']['dt'], json['timezone'])} (local time)\n")
-
+#================================================================================================
+def present_map_for(this_location):
     # Create a Folium map around the location
-    m = folium.Map(location=[ json['lat'],json['lon'] ], zoom_start=13)
+    m = folium.Map(location=[this_location['lat'], this_location['lon']], zoom_start=13)
 
     # Add a marker at the location
-    folium.Marker(location=[ json['lat'],json['lon'] ], popup="This is a marker", tooltip="Click me").add_to(m)
+    folium.Marker(location=[this_location['lat'], this_location['lon']], popup="This is a marker", tooltip="Click me").add_to(m)
 
     # Display the map in Streamlit and get user interaction data
     output = st_folium(m, width=700, height=500)
+
+#=================================================================================================
+def present_weather_data_for(location):
+    #    print(json['current']['weather'][0]['icon'])
+
+    # Print the weather data for the location
+    st.write(f"Weather Conditions") #in {location}:")
+    st.write(f"{location['current']['weather'][0]['description']}, {int(location['current']['temp'] - 273.15)}C, {location['current']['humidity']}% humidity")
+    st.write(f"{get_local_datetime(location['current']['dt'], location['timezone'])} (local time)\n")
+
+#=================================================================================================
+def preset_weather_for(this_location):
+
+    location = get_weather_data_for(this_location)
+    present_weather_data_for(location)
+    present_map_for(location)
 
 #================================================================================================
 def get_local_datetime(utc_timestamp, loca_timezone):
@@ -67,7 +78,6 @@ def get_json_from_file(filename):
 #=================================================================================================
 def get_favorite_locations():
     return get_json_from_file(FAVORITE_LOCATIONS_FILE)
-
 
 
 

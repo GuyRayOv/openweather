59257dd2fc92065af6823d7162bd86b50b80156e
GuyRayOv <guy.ray@gmail.com>
2025-07-30 16:49:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import datetime
 import pytz
 import json
 import folium
-import streamlit_folium as st_folium
+from streamlit_folium import st_folium
 
 #================================================================================================
 GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct"
@@ -33,14 +33,25 @@ def get_weather_data_for(location):
     json2 = webapi_call(WEATHER_BASE_URL, params={"lat":json1[0]["lat"], "lon":json1[0]["lon"], "appid": MY_API_KEY})
     if json2 == []: st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
 
-    print(json2)
     return json2
 
 #=================================================================================================
 def preset_weather_for(location):
+
     json = get_weather_data_for(location)
+
+    # Print the weather data for the location
     st.write(f"Weather Conditions in {location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp'] - 273.15)}C, {json['current']['humidity']}% humidity")
     st.write(f"{get_local_datetime(json['current']['dt'], json['timezone'])} (local time)\n")
+
+    # Create a Folium map around the location
+    m = folium.Map(location=[ json['lat'],json['lon'] ], zoom_start=13)
+
+    # Add a marker at the location
+    folium.Marker(location=[ json['lat'],json['lon'] ], popup="This is a marker", tooltip="Click me").add_to(m)
+
+    # Display the map in Streamlit and get user interaction data
+    output = st_folium(m, width=700, height=500)
 
 #================================================================================================
 def get_local_datetime(utc_timestamp, loca_timezone):
@@ -56,9 +67,6 @@ def get_json_from_file(filename):
 #=================================================================================================
 def get_favorite_locations():
     return get_json_from_file(FAVORITE_LOCATIONS_FILE)
-
-
-
 
 
 

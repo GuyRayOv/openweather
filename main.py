70a009d04b7f1d95fae0fe4cfb1c1de632960b84
GuyRayOv <guy.ray@gmail.com>
2025-07-30 16:36:43 +0300

    ok

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
+
 import streamlit as st
 import requests
 import datetime
 import pytz
 import json
 import folium
+import streamlit_folium as st_folium
 
+#================================================================================================
 GEO_BASE_URL = "http://api.openweathermap.org/geo/1.0/direct"
 WEATHER_BASE_URL = "https://api.openweathermap.org/data/3.0/onecall"
 MY_API_KEY = "c4d9b7d003d31461abe0aec452e24151"
+FAVORITE_LOCATIONS_FILE = 'favorite_locations.json'
 
 #================================================================================================
 def webapi_call(url, params={}):
@@ -18,6 +22,7 @@ def webapi_call(url, params={}):
         print(e)
         return []
     return response.json()
+
 #=================================================================================================
 def get_weather_data_for(location):
     json1 = webapi_call(GEO_BASE_URL, params={"q": location, "appid": MY_API_KEY})
@@ -28,7 +33,14 @@ def get_weather_data_for(location):
     json2 = webapi_call(WEATHER_BASE_URL, params={"lat":json1[0]["lat"], "lon":json1[0]["lon"], "appid": MY_API_KEY})
     if json2 == []: st.write(f"lat:{json1[0]["lat"]}, lon:{json1[0]["lon"]} cannt get web infomation")
 
+    print(json2)
     return json2
+
+#=================================================================================================
+def preset_weather_for(location):
+    json = get_weather_data_for(location)
+    st.write(f"Weather Conditions in {location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp'] - 273.15)}C, {json['current']['humidity']}% humidity")
+    st.write(f"{get_local_datetime(json['current']['dt'], json['timezone'])} (local time)\n")
 
 #================================================================================================
 def get_local_datetime(utc_timestamp, loca_timezone):
@@ -43,19 +55,21 @@ def get_json_from_file(filename):
 
 #=================================================================================================
 def get_favorite_locations():
-    return get_json_from_file('favorite_locations.json')
+    return get_json_from_file(FAVORITE_LOCATIONS_FILE)
 
-#=================================================================================================
-def preset_weather_for_location(location):
-    json = get_weather_data_for(location)
-    st.write(f"Weather Conditions in {location}: {json['current']['weather'][0]['description']}, {int(json['current']['temp'] - 273.15)}C, {json['current']['humidity']}% humidity")
-    st.write(f"{get_local_datetime(json['current']['dt'], json['timezone'])} (local time)\n")
+
+
+
+
+
 
 
 st.title('Weather App')
 
-for location in get_favorite_locations().keys(): preset_weather_for_location(location)
+for location in get_favorite_locations().keys():
+    preset_weather_for(location)
 
 location = st.text_input('Enter a location name', '')
-if location: preset_weather_for_location(location)
+if location:
+    preset_weather_for(location)
 
